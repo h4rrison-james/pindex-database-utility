63d57c8cd71b5232367f4cfe888a7bc4cde7cb38
Harrison Sweeney <h4rrison.james@gmail.com>
2014-02-27 13:52:57 +0800

    Added cities information in Excel format. Need to solve unicode issue.

--- Pindex Utility/main.m
+++ Pindex Utility/main.m
@@ -64,6 +64,13 @@ int main(int argc, const char * argv[])
             NSLog(@"Error while saving %@", ([error localizedDescription] != nil) ? [error localizedDescription] : @"Unknown Error");
             exit(1);
         }
+        
+        NSError *err = nil;
+        NSString* dataPath = [[NSBundle mainBundle] pathForResource:@"cities" ofType:@"json"];
+        NSArray* cities = [NSJSONSerialization JSONObjectWithData:[NSData dataWithContentsOfFile:dataPath]
+                                                         options:kNilOptions
+                                                           error:&err];
+        NSLog(@"Imported Cities: %@", cities);
     }
     return 0;
 }
